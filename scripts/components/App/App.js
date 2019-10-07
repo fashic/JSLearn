ebfb55b8012ab1148ea96bd3e405aca1ad8b0e08
Alex Linker <fashic@bk.ru>
2019-10-07 21:29:41 +1000

    Add margin to main title

--- scripts/components/App/App.js
+++ scripts/components/App/App.js
@@ -61,7 +61,7 @@ export class App {
     this._el.innerHTML = `
       <div class="row">
         <div class="col s12">
-            <h1>Tiny Crypto Market</h1>
+            <h1 class="main-title">Tiny Crypto Market</h1>
         </div>
       </div>
       <div class="row portfolio-row">
